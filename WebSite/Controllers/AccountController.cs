4e4356bc3eaf8a4c6b78c95318c0f1c46f235c04
ssswagatss <ssswagatss@gmail.com>
2015-10-28 19:09:17 +0530

    Login COmpleted

---
+++ WebSite/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebSite.Models;
+
+namespace WebSite.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+            SuccessLoginResponse loginData = new SuccessLoginResponse();
+            using (var client = new HttpClient())
+            {
+                var values = new Dictionary<string, string>
+                {
+                    { "UserName", model.UserName },
+                    { "Password", model.Password },
+                    { "grant_type", "password"}
+                };
+
+                var content = new FormUrlEncodedContent(values);
+                var response =await client.PostAsync ("http://localhost:54486/Token", content);
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+                    //var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+                    //var jsonObject = serializer.DeserializeObject(responseString);
+
+                    loginData = JsonConvert.DeserializeObject<SuccessLoginResponse>(responseString);
+
+                    FormsAuthentication.Initialize();
+                    FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1,
+                        loginData.UserName,
+                        DateTime.Now,
+                        DateTime.Now.AddMinutes(30), // value of time out property
+                        false, // Value of IsPersistent property
+                        loginData.AccessToken,
+                        FormsAuthentication.FormsCookiePath);
+
+                    string encryptedTicket = FormsAuthentication.Encrypt(ticket);
+                    HttpCookie cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+                    if (ticket.IsPersistent)
+                    {
+                        cookie.Expires = ticket.Expiration;
+                    }
+                    Response.Cookies.Add(cookie);
+                    Session["UserInfo"] = loginData;
+                }
+            }
+                    return Json(loginData);
+        }
+    }
+}
---
+++ WebSite/Models/SuccessLoginResponse.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSite.Models
+{
+    public class SuccessLoginResponse
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonProperty("userName")]
+        public string UserName { get; set; }
+
+        [JsonProperty(".issued")]
+        public string Issued { get; set; }
+
+        [JsonProperty(".expires")]
+        public string Expires { get; set; }
+    }
+}
